81491bf6ddc8c46e7377be2b2d443a58e600202d
Travis E <teubanks@tutelaholding.com>
2015-12-17 11:53:13 -0600

    adds type column and adjust forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-
   resources :expenses
+
+  root 'expenses#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151217164812) do
+ActiveRecord::Schema.define(version: 20151217170011) do
 
   create_table "expenses", force: :cascade do |t|
     t.string   "cost"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151217164812) do
     t.integer  "total_miles"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "expense_type"
   end
 
 end
