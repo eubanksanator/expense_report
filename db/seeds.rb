e752bb1f541e94368f94d46da70f657d25655807
Travis E <teubanks@tutelaholding.com>
2016-01-04 12:21:17 -0600

    adds departments to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 Expense.destroy_all
 ExpenseType.destroy_all
 Occurrence.destroy_all
+Department.destroy_all
+Employee.destroy_all
 
 ExpenseType.create([
   { name: "Travel", gl_code: 7580 },
@@ -18,14 +20,22 @@ ExpenseType.create([
   { name: "Client Entertainment", gl_code: 7555 }
   ])
 
+10.times do
+  Department.create(name: "IT", manager: Faker::Name.name, email: Faker::Internet.email)
+  Department.create(name: "HR", manager: Faker::Name.name, email: Faker::Internet.email)
+  Department.create(name: "Accounting", manager: Faker::Name.name, email: Faker::Internet.email)
+end
+
 20.times do
+  @department_id = Department.all.sample.id
   Occurrence.create(occur: Faker::Date.between(30.days.ago, Date.today))
-  Employee.create(name: Faker::Name.name, email: Faker::Internet.email, password: "12345678", password_confirmation: "12345678")
+  Employee.create(name: Faker::Name.name, department_id: @department_id,email: Faker::Internet.email, password: "12345678", password_confirmation: "12345678")
 
 end
 
 @occurrences = Occurrence.all
 @ex_type = ExpenseType.all
+@employee = Employee.all
 
 @occurrences.each do |occur|
   Expense.create(
@@ -34,6 +44,7 @@ end
     reason: Faker::Company.catch_phrase,
     person_entertained: Faker::Name.name,
     occurrence_id: occur.id,
+    employee_id: @employee.sample.id,
     expense_type_id: @ex_type.sample.id )
 end
 
@@ -45,4 +56,5 @@ puts "Created #{ExpenseType.count} Expense Types"
 puts "Created #{Occurrence.count} Occurrences"
 puts "Created #{Expense.count} Expenses"
 puts "Created #{Employee.count - 1} Employees"
+puts "Created #{Department.count} Departments"
 
